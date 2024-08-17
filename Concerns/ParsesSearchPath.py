3e7e95f9fa677709db04f74d635f30b242a1f469
arifikimiaunila <arifikimiaunila@gmail.com>
2024-08-17 21:33:31 +0700

    Update ParsesSearchPath.py

--- Concerns/ParsesSearchPath.py
+++ Concerns/ParsesSearchPath.py
@@ -5,12 +5,10 @@ class ParsesSearchPath:
     #Parse the Postgres "search_path" configuration value into an array.
     #@param  string|list|null  searchPath
     #@return list
-    def parseSearchPath(searchPath, matches, schema):
+    def parseSearchPath(searchPath):
       if type(searchPath) is str:
-          matches = re.findall('/[^\s,"\']+/', searchPath)
-          searchPath = matches[0]
-        
-      if type(searchPath) is not None:
-          return list(str.strip(schema))
+          x=re.search('/[^\s,"\']+/', searchPath)
+      if x:
+          return searchPath
       else:
           return []
